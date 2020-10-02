1fbf05cc8a224b074b9729ae2ca4a037128c749d
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-10-02 12:01:12 +0100

    :x: Arreglado el test anterior, pero el primer test ahora falla cuando el número es 0

--- kotlin/src/FizzBuzz/FizzBuzz.kt
+++ kotlin/src/FizzBuzz/FizzBuzz.kt
@@ -4,6 +4,10 @@ package FizzBuzz
 
 class FizzBuzz {
     fun get (number:Int) : String {
+        
+        if (number % 3 == 0) {
+            return "Fizz"
+        }
         return number.toString()
     }
 }
