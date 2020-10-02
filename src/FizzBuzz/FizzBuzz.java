51692c367bdc3da2875d45c252a34db51273721d
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-10-02 11:13:34 +0100

    :white_check_mark: Devuelve Fizz si contiene un 3

--- src/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzz/FizzBuzz.java
@@ -2,18 +2,19 @@ package FizzBuzz;
 
 public class FizzBuzz {
     public String get (int number) {
+        String str_number = "" + number;
         if (number == 0) {
             return "0";
         }
         if ((number % 3 == 0) && (number % 5 == 0)) {
             return "FizzBuzz";
         }
-        if (number % 3 == 0) {
+        if ((number % 3 == 0)||(str_number.contains("3"))) {
             return "Fizz";
         }
         if (number % 5 == 0) {
             return "Buzz";
         }
-        return ""+number;
+        return str_number;
     }
 }
