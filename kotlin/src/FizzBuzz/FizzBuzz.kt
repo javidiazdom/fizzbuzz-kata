07dbcd58b477e24bf0785eff5537745c372e1243
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-10-02 12:03:12 +0100

    :white_check_mark: Ahora devuelve 0 cuando el númnero es 0 en lugar de Fizz

--- kotlin/src/FizzBuzz/FizzBuzz.kt
+++ kotlin/src/FizzBuzz/FizzBuzz.kt
@@ -4,7 +4,9 @@ package FizzBuzz
 
 class FizzBuzz {
     fun get (number:Int) : String {
-        
+        if (number == 0){
+            return "0"
+        }
         if (number % 3 == 0) {
             return "Fizz"
         }
