3bf2edb6e1459543aa2eda501cef6fdab50f7449
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-10-02 12:09:42 +0100

    ::white_check_mark: Devuelve FizzBuzz cuando el número es múltiplo de 5 y 3.

--- kotlin/src/FizzBuzz/FizzBuzz.kt
+++ kotlin/src/FizzBuzz/FizzBuzz.kt
@@ -7,6 +7,9 @@ class FizzBuzz {
         if (number == 0){
             return "0"
         }
+        if ((number % 3 == 0) && (number % 5 == 0)) {
+            return "FizzBuzz"
+        }
         if (number % 3 == 0) {
             return "Fizz"
         }
